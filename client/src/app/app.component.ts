b354b964af6a305c22892beabe9af72c7dfce905
Ylli <rukiqiyll@gmail.com>
2021-06-07 13:51:59 +0200

    End of season 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+import { IProduct } from './_models/products';
+import { IPagination } from './_models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Skinet';
+
+  products: IProduct[] | any;
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: IPagination | any) => {
+      this.products = response.data;
+    }, error => {
+      console.log(error);
+    });
+  }
 }
